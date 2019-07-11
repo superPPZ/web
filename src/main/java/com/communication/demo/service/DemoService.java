4c1955deb80802d1f19ae578b45fe3fd0ca02d30
ppz <15131035435@163.com>
2019-07-11 15:25:40 +0800

    创建develop分支并且提交

--- src/main/java/com/communication/demo/service/DemoService.java
+++ src/main/java/com/communication/demo/service/DemoService.java
@@ -12,49 +12,52 @@ import com.communication.demo.mapper.DemoMapper;
 @Service
 public class DemoService {
 
-	@Autowired
-	private DemoMapper demoMapper;
-	
-	/**
-	 * 需要开启事务.使用注解的方式.
-	 * @param demo
-	 * @return
-	 */
-	@Transactional
-	public int save(Demo demo){
-		return demoMapper.save(demo);
-	}
-	
-	@Transactional
-	public int update(Demo demo){
-		return demoMapper.update(demo);
-	}
-	
-	@Transactional
-	public int delete(int id){
-		return demoMapper.delete(id);
-	}
-	
-	
-	public List<Demo> selectAll(){
-		return demoMapper.selectAll();
-	}
-	
-	
-	public Demo selectById(int id){
-		return demoMapper.selectById(id);
-	}
-	
-	public List<Demo> select1(String name,String email){
-		return demoMapper.select1(name,email);
-	}
-	
-	
-	public List<Demo> selectByName(String name){
-		return demoMapper.selectByName(name);
-	}
-	
-	public List<Demo> selectByName2(String name){
-		return demoMapper.selectByName2(name);
-	}
+    @Autowired
+    private DemoMapper demoMapper;
+
+    /**
+     * 需要开启事务.使用注解的方式.
+     *
+     * @param demo
+     * @return
+     */
+    @Transactional
+    public int save(Demo demo) {
+        return demoMapper.save(demo);
+    }
+
+    @Transactional
+    public int update(Demo demo) {
+        return demoMapper.update(demo);
+    }
+
+    @Transactional
+    public int delete(int id) {
+        return demoMapper.delete(id);
+    }
+
+
+    public List<Demo> selectAll() {
+
+        System.out.println("yes");
+        return demoMapper.selectAll();
+    }
+
+
+    public Demo selectById(int id) {
+        return demoMapper.selectById(id);
+    }
+
+    public List<Demo> select1(String name, String email) {
+        return demoMapper.select1(name, email);
+    }
+
+
+    public List<Demo> selectByName(String name) {
+        return demoMapper.selectByName(name);
+    }
+
+    public List<Demo> selectByName2(String name) {
+        return demoMapper.selectByName2(name);
+    }
 }
